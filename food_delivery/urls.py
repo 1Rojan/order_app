1788cd3bf9781971ebc6b6e11f85a24462fb19fd
1Rojan <rznsnr11@gmail.com>
2022-09-21 13:09:43 +0545

    template setup for customer and restaurant app

--- food_delivery/urls.py
+++ food_delivery/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from unicodedata import name
 from django.contrib import admin
 from django.urls import path
+from customer.views import Index, About
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Index.as_view(), name='index'),
+    path('about/', About.as_view(), name='about')
 ]
