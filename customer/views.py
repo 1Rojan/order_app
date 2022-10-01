b4563823f7ff25445ab096badbef05e0ef84922e
1Rojan <rznsnr11@gmail.com>
2022-10-01 12:53:57 +0545

    added fields for order, refactored template

--- customer/views.py
+++ customer/views.py
@@ -19,11 +19,11 @@ class Order(View):
 
   def get(self, request, *args, **kwargs):
     categories = Category.objects.all()
-    return render(request, 'customer/place_order.html', {'categories':categories})
+    return render(request, 'customer/order.html', {'categories':categories})
 
   def post(self, request, *args, **kwargs):
     order_items = []
-    order_item_ids = request.POST.getlist('item[]')
+    order_item_ids = request.POST.getlist('items[]')
     for item_id in order_item_ids:
       item = MenuItem.objects.get(pk__contains=item_id)
       item_data = {
@@ -39,9 +39,14 @@ class Order(View):
     for item in order_items:
       item_ids.append(item['pk'])
       price += item['price']
-    order = OrderModel.objects.create(price=price)
+    order = OrderModel.objects.create(
+      price=price,
+      email=request.POST.get('email'),
+      street=request.POST.get('street'),
+      city=request.POST.get('city'),
+      state=request.POST.get('state')
+      )
     order.item.add(*item_ids)
-
     context = {
       'orders': order
     }
